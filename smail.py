ab2d58061b4429254aa2659c6bb7c4b943201806
Tiger-Tom <55163690+Tiger-Tom@users.noreply.github.com>
2019-12-10 14:45:56 -0800

    Update smail.py

--- smail.py
+++ smail.py
@@ -6,5 +6,23 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-def start():
+gmail = ['smtp.gmail.com', 587]
+
+outlook = ['smtp.live.com', 587]
+
+office365 = ['smtp.office365.com', 587]
+
+yahoo = ['smtp.mail.yahoo.com', 465]
+yahoo_plus = ['plus.smtp.yahoo.com', 465]
+yahoo_uk = ['smtp.yahoo.co.uk', 465]
+yahoo_deutschland = ['smtp.mail.yahoo.com', 465]
+yahoo_au = ['smtp.mail.yahoo.com.au', 465]
+yahoo_nz = ['smtp.mail.yahoo.com.au', 465]
+
+o2 = ['smtp.o2.ie', 25]
+o2_uk = ['smtp.o2.co.uk', 25]
+
+aol = ['smtp.aoi.com', 587]
+
+def startGmail(type):
 def send()
