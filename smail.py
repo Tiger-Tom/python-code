45e8cab6dc496d44f47e739df91ddccb9d1f96a6
Tiger-Tom <55163690+Tiger-Tom@users.noreply.github.com>
2019-12-10 14:35:05 -0800

    Create smail.py

---
+++ smail.py
@@ -0,0 +1,10 @@
+import smtplib
+
+import email
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def start():
+def send()
